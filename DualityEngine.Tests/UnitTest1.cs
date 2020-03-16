37fcb69f41a4b2c7b9be0c7e30721bb2280b4616
Kristyan Yochev <kris.yochev@gmail.com>
2020-03-16 02:13:22 +0200

    [Fix] Travis.yml tweaks

--- DualityEngine.Tests/UnitTest1.cs
+++ DualityEngine.Tests/UnitTest1.cs
@@ -1,11 +1,15 @@
-using System;
-using Xunit;
+using NUnit.Framework;
 
 namespace DualityEngine.Tests
 {
-    public class UnitTest1
+    public class Tests
     {
-        [Fact]
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
         public void Test1()
         {
             Assert.True(true);
