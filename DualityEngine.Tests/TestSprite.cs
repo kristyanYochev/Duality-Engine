cc7ca41beb98e59c1f93023a5bd1c0f37fc8f556
Radevskii <37086021+Radevskii@users.noreply.github.com>
2020-03-21 00:09:47 +0200

    [Update] Added UTests for Sprite.GetCharAt

---
+++ DualityEngine.Tests/TestSprite.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using DualityEngine.Graphics;
+namespace DualityEngineTests
+{
+    [TestFixture]
+    public class TestSprite
+    {
+        Sprite testSprite;
+
+        [SetUp]
+        public void SetUp()
+        {
+            testSprite = new Sprite("*",1,1);
+        }
+
+        [Test]
+        public void TestGetCharAt()
+        {
+            Assert.AreEqual('*', testSprite.GetCharAt(0, 0));
+        }
+
+        [Test]
+        public void TestInvalidGetCharAt()
+        {
+            Assert.Throws<Exception>(()=>testSprite.GetCharAt(10, 10));
+        }
+    }
+}
