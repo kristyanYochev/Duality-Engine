5bfd9d402a18b6275362900490b6e3eddccb2a5e
Radevskii <37086021+Radevskii@users.noreply.github.com>
2020-03-21 13:15:13 +0200

    [Update] Added IConsole interface

---
+++ DualityEngine/Intefaces/IConsole.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace DualityEngine.Interfaces
+{
+    interface IConsole
+    {
+        bool CursorVisible { get; set; }
+        bool KeyAvailable { get; set; }
+        bool TreatControlCAsInput { get; set; }
+        int WindowWidth { get; set; }
+        int WindowHeight { get; set; }
+        void Clear();
+        Stream OpenStandardOutput();
+        ConsoleKeyInfo ReadKey();
+        ConsoleKeyInfo ReadKey(bool intercept);
+        void SetIn(TextReader newIn);
+    }
+}
