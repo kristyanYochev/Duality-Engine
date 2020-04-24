382a99fa59a04df80bb16e21a5b6bea00441bf12
Kristyan Yochev <kris.yochev@gmail.com>
2020-04-24 15:54:52 +0300

    Fixed the logger not closing

--- ShowCase/Program.cs
+++ ShowCase/Program.cs
@@ -12,17 +12,19 @@ namespace ShowCase
     {
         static void Main(string[] args)
         {
+            DualityEngine.Debug.Instance.SetUp("/Users/kristyanyochev/Projects/DualityEngine/ShowCase/Logs.log");
             Scene mainScene = new Scene();
-            GameObject stick = new GameObject(new DualityEngine.Mathf.Vector2 { x = 0.5f, y = 0.5f }, mainScene);
-            stick.AddComponent(new AnimationController(stick, Animation.ParseAnimation("Sprites/Animes.json")));
-            mainScene.AddObject(stick);
-            //GameObject dinosaur = new GameObject(new DualityEngine.Mathf.Vector2 { x = 0.5f, y = 0.7f }, mainScene);
-            //dinosaur.AddComponent(new SpriteRenderer(dinosaur, Sprite.ParseSprite("Sprites/Dino.txt")));
-            //dinosaur.AddComponent(new DinoController(dinosaur));
+            //GameObject stick = new GameObject(new DualityEngine.Mathf.Vector2 { x = 0.5f, y = 0.5f }, mainScene);
+            //stick.AddComponent(new AnimationController(stick, Animation.ParseAnimation("Sprites/Animes.json")));
+            //mainScene.AddObject(stick);
+            GameObject dinosaur = new GameObject(new DualityEngine.Mathf.Vector2 { x = 0.5f, y = 0.7f }, mainScene);
+            dinosaur.AddComponent(new SpriteRenderer(dinosaur, Sprite.ParseSprite("Sprites/Dino.txt")));
+            dinosaur.AddComponent(new DinoController(dinosaur));
 
-            //mainScene.AddObject(dinosaur);
+            mainScene.AddObject(dinosaur);
 
             mainScene.Run();
+            DualityEngine.Debug.Instance.TearDown();
         }
     }
 }
