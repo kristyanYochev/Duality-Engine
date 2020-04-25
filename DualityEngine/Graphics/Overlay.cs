013b20b094a3f10d8fb045c812ea7324f7e6c9ac
alexuzunov <alex.uzunov2002@gmail.com>
2020-04-25 23:43:44 +0300

    [Update] Added UI elements and scene overlay

---
+++ DualityEngine/Graphics/Overlay.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DualityEngine.Exceptions;
+
+namespace DualityEngine.Graphics
+{
+    public class Overlay
+    {
+        private Dictionary<string, UIElement> Elements { get; set; }
+
+        public Overlay()
+        {
+            Elements = new Dictionary<string, UIElement>();
+        }
+
+        public void AddElement(string name, UIElement element)
+        {
+            Elements.Add(name, element);
+        }
+
+        public UIElement GetElement(string name)
+        {
+            UIElement element;
+            if (!Elements.TryGetValue(name, out element))
+            {
+                throw new UIElementNotFoundException();
+            }
+            return element;
+        }
+
+        public void Render()
+        {
+            foreach(var element in Elements)
+            {
+                element.Value.Render();
+            }
+        }
+    }
+}
